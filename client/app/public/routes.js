4ad97090d8bdf4e75b21ea0d35065225d9b0b99e
Bilal <blalsnmez.8@gmail.com>
2022-04-03 17:40:45 +0300

    Front Core

--- client/app/public/routes.js
+++ client/app/public/routes.js
@@ -3,6 +3,20 @@ import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 FlowRouter.route('/', {
   name: 'public.home',
   action: function (params, queryParams) {
-    this.render('publicLayoutDefault');
+    this.render('publicLayoutDefault', {page: 'publicPagesHome'});
+  }
+});
+
+FlowRouter.route('/about', {
+  name: 'public.about',
+  action: function (params, queryParams) {
+    this.render('publicLayoutDefault', {page: 'publicPagesAbout'});
+  }
+});
+
+FlowRouter.route('/contact', {
+  name: 'public.contact',
+  action: function (params, queryParams) {
+    this.render('publicLayoutDefault', {page: 'publicPagesContact'});
   }
 });
