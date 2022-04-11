61967c0d0420aa61171bb548da536ab3e08fd439
AykutSefali <102411696+AykutSefali@users.noreply.github.com>
2022-04-11 22:31:13 +0300

    page

---
+++ client/app/public/pages/home/index.js
@@ -0,0 +1,9 @@
+Template.publicPagesHome.events({
+    
+    "click .btnMore": function (event, template) {
+      let moretext=document.getElementById("more");
+           moretext.style.display = "inline";
+    
+      },
+   
+  });
