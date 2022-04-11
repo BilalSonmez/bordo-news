2462f7d555c29a768de09b5f93c51fc48c9d52f8
Nuhylmz <senturknuhyilmaz@gmail.com>
2022-04-11 23:53:15 +0300

    fix

--- server/methods/category/update.js
+++ server/methods/category/update.js
@@ -3,7 +3,7 @@ import SimpleSchema from 'simpl-schema';
 new ValidatedMethod({
   name: 'category.update',
   mixins : [SignedInMixin,RoleMixin],
-  roles: ["roles.admin"],
+  roles: ["roles.editor"],
   validate: new SimpleSchema({
     _id: SimpleSchema.RegEx.Id,
     category: CategorySchema
