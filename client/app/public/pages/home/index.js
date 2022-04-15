ea18c14d263f0a2d834f73ba5fc72435f9f3a955
Nuhylmz <senturknuhyilmaz@gmail.com>
2022-04-15 03:26:22 +0300

    homepage some fixes

--- client/app/public/pages/home/index.js
+++ client/app/public/pages/home/index.js
@@ -1,9 +1,61 @@
-Template.publicPagesContent.events({
-    
-    "click .btnMore": function (event, template) {
-      let moretext=document.getElementById("more");
-           moretext.style.display = "inline";
-    
-      },
-   
+Template.publicPagesHome.onCreated(function () {
+  this.state = new ReactiveDict(null, {
+    breakingNews: [],
   });
+
+  this.pagination = new ReactiveDict(null, {
+    currentPage: 1,
+    pageItems: 5,
+    totalCount: 0,
+    totalPages: 0
+  });
+
+  this.sorting = new ReactiveDict(null, {
+    sortField: 'name',
+    sortOrder: 'asc'
+  });
+
+  this.filtering = new ReactiveDict(null, {});
+});
+
+Template.publicPagesHome.onRendered(function () {
+  const self = this;
+
+  const listOptions = {
+    options: {
+      filtering: {  isImportant: { $eq: true }  },
+      sorting: {
+        sortField: 'createdAt',
+        sortOrder: 'desc',
+      }
+    }
+  };
+  this.autorun(function () {
+    Meteor.call("news.list",listOptions, function (error, result) {
+      if (error) {
+        ErrorHandler.show(error.message);
+        return;
+      }
+      console.log(result);
+      self.state.set("breakingNews", result.news);
+    })
+  });
+});
+
+
+Template.publicPagesHome.events({
+  'mouseenter .btnCarousel': function (event, template) {
+    $(event.target).click();
+  },
+  "click .btnMore": function (event, template) {
+    let moretext = document.getElementById("more");
+    if (moretext.style.display == "none" ) {
+      moretext.style.display = "inline";
+      //moretext.value ="Daha Az Göster";
+    }else{
+      moretext.style.display = "none";
+      //moretext.value ="Daha Fazla Göster";
+    }
+  },
+
+});
